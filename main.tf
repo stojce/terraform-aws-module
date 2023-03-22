13eb8dad23ab37b7aa5ce2faeabf51b14840c42b
stojce <stojce@me.com>
2023-03-22 09:29:51 +0100

    build: initial
    Signed-off-by: stojce <stojce@me.com>

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ variable "map_example" {
     type = map(string)
     description = "value of map"
 }
+
+variable "list_example" {
+    type = list(string)
+    description = "value of list"
+}
+
+variable "object_example" {
+    type = object
+    description = "value of object"
+}
