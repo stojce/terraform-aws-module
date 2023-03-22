06efe304c799a5f32cd9c3a78849130ed8725744
Stojce Slavkovski <stojce@me.com>
2023-03-22 09:33:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,18 @@
+
 variable "map_example" {
-    type = map(string)
-    description = "value of map"
+  type        = map(string)
+  description = "value of map"
 }
 
 variable "list_example" {
-    type = list(string)
-    description = "value of list"
+  type        = list(string)
+  description = "value of list"
 }
 
 variable "object_example" {
-    type = object
-    description = "value of object"
+  type = object({
+    key   = string
+    value = map(string)
+  })
+  description = "value of object"
 }
